5a4a7ef2a2da98e54127ee054c45b5b8b3aae543
pikapikapow <vladimirhwang84@gmail.com>
2024-10-14 10:29:42 -0400

    trivia game base finished

---
+++ script.js
@@ -0,0 +1,26 @@
+fetch("data.json")
+  .then((response) => response.json())
+  // .then((data) => console.log(data));
+  .then((data) => displayQuestions(data));
+
+// fetchData();
+
+function displayQuestions(data) {
+  const container = document.getElementById("questions-container");
+  data.forEach((item) => {
+    const questionDiv = document.createElement("div");
+    questionDiv.classList.add("question");
+
+    questionDiv.innerHTML = `
+            <h3>${item.category}</h3>
+            <p>Question: ${item.question}</p>
+            <p>Options:</p>
+            <ul>
+                ${item.options.map((option) => `<li>${option}</li>`).join("")}
+            </ul>
+            <p>Answer: ${item.answer}</p>
+        `;
+
+    container.appendChild(questionDiv);
+  });
+}
